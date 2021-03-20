74c1e0e6b28cd95a92885008fd82f8b6e822fe2f
qasimmehdi1 <qasim.sourcecode@gmail.com>
2021-03-20 16:04:38 +0500

    search added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { TwitterTweetEmbed } from "react-twitter-embed";
@@ -5,12 +6,18 @@ import "./App.css";
 
 function App() {
   const [tweets, setTweets] = useState([]);
+  const [noTweets, setNoTweets] = useState(false);
+  const search = window.location.pathname.split("/")[1];
+
   useEffect(() => {
     axios
       .get(
-        "http://ec2-13-58-153-32.us-east-2.compute.amazonaws.com:8080/sentiment"
+        `http://ec2-13-58-153-32.us-east-2.compute.amazonaws.com:8080/sentiment?search=${search}`
       )
-      .then((res) => setTweets(res.data))
+      .then((res) => {
+        setTweets(res.data);
+        !res.data.length > 0 ? setNoTweets(true) : setNoTweets(false);
+      })
       .catch((err) => console.log(err));
   }, []);
 
@@ -22,11 +29,13 @@ function App() {
   return (
     <div className="App">
       {tweets.map((tweet, i) => (
-        <TwitterTweetEmbed
-          key={"tweet_" + i}
-          tweetId={getId(tweet.link)}
-        />
+        <TwitterTweetEmbed key={"tweet_" + i} tweetId={getId(tweet.link)} />
       ))}
+      {noTweets && (
+        <div style={{ color: "white", fontWeight: "600", textAlign: "center" }}>
+          No tweets found!
+        </div>
+      )}
     </div>
   );
 }
